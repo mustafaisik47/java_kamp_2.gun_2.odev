903f55d8219f22d5725b958e0c85cde00aa2d37f
Mustafa IŞIK <71766424+mustafaisik47@users.noreply.github.com>
2021-05-09 03:43:24 +0300

    Add files via upload

---
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -0,0 +1,9 @@
+package classesWithAttributes;
+
+public class ProductManager {
+	public void add( Product product) {
+		//JDBS
+		System.out.println("Ürün EKlendi : " + product.getName() );
+		
+	}
+}
---
+++ methodOverloading/src/methodOverloading/Mian.java
@@ -0,0 +1,10 @@
+package methodOverloading;
+
+public class Mian {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.topla(7, 54));
+		System.out.println(dortIslem.topla(1, 2, 3));
+	}
+}
